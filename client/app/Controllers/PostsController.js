396807760df9d7556d15b330895ce5a7ae678e3b
Katie McCauley <katiemccau02@gmail.com>
2022-03-04 16:12:09 -0700

    commit

--- client/app/Controllers/PostsController.js
+++ client/app/Controllers/PostsController.js
@@ -1,5 +1,47 @@
-export class PostsController{
-    constructor() {
-        
+import { ProxyState } from "../AppState.js"
+import { postsService } from "../Services/PostsService.js"
+import { logger } from "../Utils/Logger.js"
+
+async function _getAllPosts() {
+    try {
+        await postsService.getAllPosts()
+    } catch (error) {
+        logger.error(error.message)
     }
+}
+
+function _drawPosts() {
+    let template = ""
+    ProxyState.posts.forEach(p => template += p.Template)
+    document.getElementById('posts').innerHTML = template
+}
+
+export class PostsController {
+
+
+    constructor() {
+
+        ProxyState.on("posts", _drawPosts)
+        _getAllPosts()
+    }
+
+    async createPost() {
+        try {
+            window.event.preventDefault()
+            let form = window.event.target
+            let rawData = {
+                // @ts-ignore
+                imgUrl: form.imgUrl.value,
+                // @ts-ignore
+                description: form.description.value
+            }
+            await postsService.createPost(rawData)
+            let modal = document.getElementById("create-post")
+            form.reset()
+
+        } catch (error) {
+
+        }
+    }
+
 }
